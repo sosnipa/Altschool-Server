607cddbd7aa6863876e93fe2ba312d8f5f12afb9
Leonardo Iwuoha <leonardkamsi@gmail.com>
2024-03-20 05:02:04 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const hostname = "localhost";
+const port = 8900;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end(`Hello! My name is Leonardo Kamsi`); // Replace 'yourName' with your actual name
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
